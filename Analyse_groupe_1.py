c93bc1fff45200fa2b7571649c46a1abf2569663
armeirlaen <81042055+armeirlaen@users.noreply.github.com>
2022-03-15 10:18:15 +0100

    Plot basique

--- Analyse_groupe_1.py
+++ Analyse_groupe_1.py
@@ -9,29 +9,98 @@ import coda_toolbox as coda
 import friction_toolbox as fri
 import glm_toolbox as glm
 import signal_processing_toolbox as sig
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 """
 Partie du code sur les fichier CODA
 """
 
-filename = "Groupe 1_CODA/GBIO_2022_Group_1_S1_20220008_003.TXT"
+filename = "C:/Users/arthu/Documents/Résultats_Labo_0803/Groupe 1_CODA/GBIO_2022_Group_1_S1_20220008_003.TXT"
 marker_manipulandum = [5,6,7,8] #les markers utilisés sur le manipulandum
-df = coda.import_data(filename)  #on import les données
 
-mc = coda.manipulandum_center(df, markers_id = marker_manipulandum ) #on calcule les coordonnées du centre du manipulandum
+numcoord =[3,4,6,7,8,9]
+numforce =[i for i in range(1,7)]
 
+def printCoord(pa):
+    df = coda.import_data(pa)
+    mc= coda.manipulandum_center(df, markers_id = marker_manipulandum )
+    x=mc[0]
+    y=mc[1]
+    z=mc[2]
+    return x,y,z
+
+num =[3,4,6,7,8,9]
+
+k=0
+for i in num:
+    k+=1
+    pat= "C:/Users/arthu/Documents/Résultats_Labo_0803/Groupe 1_CODA/GBIO_2022_Group_1_S1_20220008_00"+str(i)+".TXT"
+    x,y,z = printCoord(pat)
+    plt.subplot(6,1,k)
+    time=np.linspace(0,51,len(x))
+    plt.plot(time,x)
+    plt.title("x:00"+str(i))
+
+plt.show()
+
+k=0
+for i in num:
+    k+=1
+    pat= "C:/Users/arthu/Documents/Résultats_Labo_0803/Groupe 1_CODA/GBIO_2022_Group_1_S1_20220008_00"+str(i)+".TXT"
+    x,y,z = printCoord(pat)
+    plt.subplot(6,1,k)
+    time=np.linspace(0,51,len(y))
+    plt.plot(time,y)
+    plt.title("y:00"+str(i))
+
+plt.show()
 """
 Partie du code sur les fichier du manipulandum(.glm)
 """
 
-file = "Groupe 1/Bloc_S1_001.glm"
+file = "C:/Users/arthu/Documents/Résultats_Labo_0803/Groupe 1/Bloc_S1_006.glm"
 baseline = [0,1,2]
 
 glm_df = glm.import_data(file)
 
-print(glm_df)
+
+def printForce(df):
+    time= df.time
+    x= df.Fxal
+    y= df.Fyal
+    LF=(x**2+y**2)**0.5
+    GF= df.Fzal
+    return time,LF,GF
+    
+num = numforce
+
+k=0
+for i in num:
+    k+=1
+    path="C:/Users/arthu/Documents/Résultats_Labo_0803/Groupe 1/Bloc_S1_00"+str(i)+".glm"
+    curr_df = glm.import_data(path)
+    t,LFi,GFi= printForce(curr_df)
+    plt.subplot(6,1,k)
+    plt.plot(t,GFi)
+    plt.title("GF:00"+str(i))
+
+plt.show()
+
+k=0
+for i in num:
+    k+=1
+    path="C:/Users/arthu/Documents/Résultats_Labo_0803/Groupe 1/Bloc_S1_00"+str(i)+".glm"
+    curr_df = glm.import_data(path)
+    t,LFi,GFi= printForce(curr_df)
+    plt.subplot(6,1,k)
+    plt.plot(t,LFi)
+    plt.title("LF:00"+str(i))
+
+plt.show()
 
 CPL,CPR = glm.compute_cop(glm_df,baseline)
 
-print(CPL)
+print("CPL",CPL)
