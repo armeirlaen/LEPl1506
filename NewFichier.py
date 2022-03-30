38ec4e8f996b2ff33c37a1d64dc2966a09ea82fa
RoSchyns <99277488+RoSchyns@users.noreply.github.com>
2022-03-30 14:59:42 +0200

    Add files via upload

---
+++ NewFichier.py
@@ -0,0 +1,34 @@
+import coda_toolbox as coda
+import glm_toolbox as glm
+
+
+path = "Groupe 1_CODA/GBIO_2022_Group_1_S1_20220008_004.txt"
+#makercadre = [1,2,3]
+#markermanip = [5,6,7,8]
+
+df = coda.import_data(path)
+#fd2 = glm.import_data(path2)
+
+df.drop(columns=["Marker1_Visibility", "Marker2_Visibility","Marker3_Visibility",'Marker4_Visibility','Marker5_Visibility',
+                 'Marker4_Visibility','Marker5_Visibility','Marker6_Visibility','Marker7_Visibility','Marker8_Visibility',
+                 'Marker9_Visibility','Marker10_Visibility','Marker11_Visibility','Marker4_X', 'Marker4_Y', 'Marker4_Z',
+                 'Marker9_X', 'Marker9_Y', 'Marker9_Z','Marker10_X','Marker10_Y', 'Marker10_Z',
+                 'Marker11_X', 'Marker11_Y', 'Marker11_Z'],inplace = True)
+
+df.rename(columns={'time': 'timec','Marker5_X' : 'Marker4_X','Marker5_Y' : 'Marker4_Y','Marker5_Z' : 'Marker4_Z',
+                   'Marker6_X' : 'Marker5_X','Marker6_Y' : 'Marker5_Y','Marker6_Z' : 'Marker5_Z',
+                   'Marker7_X' : 'Marker6_X','Marker7_Y' : 'Marker6_Y','Marker7_Z' : 'Marker6_Z',
+                   'Marker8_X' : 'Marker7_X','Marker8_Y' : 'Marker7_Y','Marker8_Z' : 'Marker7_Z'},inplace = True)
+
+
+print(df.columns)
+
+
+
+
+file = open("Coda/S1_002.txt",'w')
+file.write(df.to_string())
+file.close()
+
+
+
